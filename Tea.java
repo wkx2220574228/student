bb4e3af56b3e573289d8e06f74ac1cb4a01570e3
zs <zs@qq.com>
2021-01-25 15:55:23 +0800

    在公司开发教室查询

--- Tea.java
+++ Tea.java
@@ -1,8 +1,15 @@
 public class Tea{
+
 	private Integer id;
 	private String name;
 
 	public void teach(){
 		Syatem.out.println(teaName);
+    }
+
+	private int id;
+	public void teachA(){
+
+
 	}
 }
